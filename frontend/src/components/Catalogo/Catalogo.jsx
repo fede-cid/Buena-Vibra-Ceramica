63237e883c722dc5475e3bb88dcdccb6d0547c73
fede-cid <fede.cid.96@gmail.com>
2023-04-05 01:23:53 -0300

    update inicio

--- frontend/src/components/Catalogo/Catalogo.jsx
+++ frontend/src/components/Catalogo/Catalogo.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import CatalogDownload from './CatalogoDownload'
-// import Catalogo from './'
+import React from 'react';
+import CatalogoStyles from './Catalogo.module.css';
+
 function Catalogo() {
+  const handleWhatsAppMessage = () => {
+    const message = encodeURIComponent('Hola, buen día. Quisiera solicitar el catálogo por mayor.');
+    const phoneNumber = '5491167879133';
+    window.open(`https://wa.me/${phoneNumber}?text=${message}`);
+  }
+
   return (
-    <div>
-      ¡Descarga nuestro catálogo ahora haciendo click aquí! Encuentra nuestras increíbles opciones al por mayor y sorprende a tus clientes con productos de alta calidad. ¡No esperes más para hacer crecer tu negocio!
-      <CatalogDownload pdfFile="" label="Mi catálogo personalizado" />
+    <div className={CatalogoStyles.container}>
+      ¡Solicita nuestro catálogo ahora haciendo click aquí! Encuentra nuestras increíbles opciones al por mayor y sorprende a tus clientes con productos de alta calidad. ¡No esperes más para hacer crecer tu negocio!
+      <button className={CatalogoStyles.button} onClick={handleWhatsAppMessage}>Solicitar catálogo por WhatsApp</button>
     </div>
-  )
+  );
 }
 
-export default Catalogo
+export default Catalogo;
