60bdf39e9dc4a7b68a47ef42e54b03c73755e4f2
fede-cid <fede.cid.96@gmail.com>
2023-04-05 02:31:44 -0300

    update cart

--- frontend/src/components/Compras/Cart.jsx
+++ frontend/src/components/Compras/Cart.jsx
@@ -12,11 +12,41 @@ const Cart = () => {
   }, []);
 
   useEffect(() => {
-    const prices = items.map(item => item.precio * item.cantidad);
-    console.log(items)
-    const totalPrice = prices.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    const prices = items.map((item) => item.precio * item.cantidad);
+    const totalPrice = prices.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
     setTotalPrice(totalPrice);
   }, [items]);
+
+  const handleDeleteItem = (itemId) => {
+    const updatedItems = items.filter((item) => item.id !== itemId);
+    setItems(updatedItems);
+    localStorage.setItem("cartItems", JSON.stringify(updatedItems));
+  };
+
+  const handleQuantityChange = (itemId, newQuantity) => {
+    const updatedItems = items.map((item) => {
+      if (item.id === itemId) {
+        return {
+          ...item,
+          cantidad: newQuantity,
+        };
+      }
+      return item;
+    });
+    setItems(updatedItems);
+    localStorage.setItem("cartItems", JSON.stringify(updatedItems));
+  };
+
+  const handleBuy = () => {
+    const productList = items
+      .map((item) => `${item.titulo} (${item.cantidad})`)
+      .join("%0A");
+    const message = `Hola buenas, quería solicitarte estos productos: %0A${productList}`;
+    window.open(`https://wa.me/123456789?text=${message}`, "_blank");
+  };
 
   return (
     <div className={styles.cart}>
@@ -30,7 +60,7 @@ const Cart = () => {
       ) : (
         <>
           <div className={styles.cartItems}>
-            {items.map(item => (
+            {items.map((item) => (
               <div key={item.id} className={styles.cartItem}>
                 <div className={styles.cartItemImg}>
                   <img src={item.img} alt={item.titulo} />
@@ -40,15 +70,42 @@ const Cart = () => {
                   <div className={styles.cartItemPrice}>
                     {item.cantidad} x {item.precio}
                   </div>
+                  <div className={styles.cartItemActions}>
+                    <button
+                      className={styles.cartItemButton}
+                      onClick={() => handleDeleteItem(item.id)}
+                    >
+                      Delete
+                    </button>
+                    <div className={styles.cartItemQuantity}>
+                      <button
+                        className={styles.cartItemButton}
+                        onClick={() =>
+                          handleQuantityChange(item.id, item.cantidad - 1)
+                        }
+                        disabled={item.cantidad === 1}
+                      >
+                        -
+                      </button>
+                    </div>
+                    <div className={styles.cartItemQuantity}>
+                      <button
+                        className={styles.cartItemButton}
+                        onClick={() =>
+                          handleQuantityChange(item.id, item.cantidad + 1)
+                        }
+                      >
+                        +
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
             ))}
+            <button className={styles.cartBuyButton} onClick={handleBuy}>
+              Buy (${totalPrice})
+            </button>
           </div>
-          <div className={styles.cartTotal}>
-            <span className={styles.cartTotalTitle}>Total:</span>
-            <span className={styles.cartTotalPrice}>${totalPrice.toFixed(2)}</span>
-          </div>
-          <button className={styles.cartCheckout}>Proceed to Checkout</button>
         </>
       )}
     </div>
--- frontend/src/context/producto/ProductoState.jsx
+++ frontend/src/context/producto/ProductoState.jsx
@@ -9,7 +9,7 @@ const ProductoState = (props) => {
       id: 1,
       nombre: "Tu compañia cada mañana",
       descripcion: "¡Despierta tus sentidos y disfruta del té en una obra de arte personalizada! Nuestros jarrones artesanales de cerámica son más que simples recipientes, son piezas únicas que reflejan tu estilo y personalidad. Cada sorbo es una experiencia única, con un jarro personalizado que demuestra que mereces lo mejor. Sumérgete en un mundo de sabor y creatividad con nuestros jarrones artesanales personalizados.",
-      imagen: "/src/recursos/jarrito.jpeg"
+      imagen: "https://github.com/fede-cid/Buena-Vibra-Ceramica/blob/main/frontend/src/recursos/jarrito.jpeg?raw=true"
     }
   }
 
