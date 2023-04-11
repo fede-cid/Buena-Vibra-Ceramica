e9b3b546c9609bf5b45bedf7ff56a52cd961c5b6
Federico Cid <104875989+fede-cid@users.noreply.github.com>
2023-04-11 10:19:09 -0300

    Update Catalogo.jsx

--- frontend/src/components/Catalogo/Catalogo.jsx
+++ frontend/src/components/Catalogo/Catalogo.jsx
@@ -10,7 +10,7 @@ function Catalogo() {
 
   return (
     <div className={CatalogoStyles.container}>
-      ¡Solicita nuestro catálogo ahora haciendo click aquí! Encuentra nuestras increíbles opciones al por mayor y sorprende a tus clientes con productos de alta calidad. ¡No esperes más para hacer crecer tu negocio!
+      ¡Solicita nuestro catálogo mayorista ahora haciendo click aquí! Encuentra nuestras increíbles opciones al por mayor y sorprende a tus clientes con productos de alta calidad. ¡No esperes más para hacer crecer tu negocio!
       <button className={CatalogoStyles.button} onClick={handleWhatsAppMessage}>Solicitar catálogo por WhatsApp</button>
     </div>
   );
