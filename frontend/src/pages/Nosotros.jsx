6aeae7397674e941121c6abecdb744ddcd73c018
fede-cid <fede.cid.96@gmail.com>
2023-04-10 22:54:07 -0300

    update 10/04

--- frontend/src/pages/Nosotros.jsx
+++ frontend/src/pages/Nosotros.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
+import Catalogo from "../components/Catalogo/Catalogo";
 function Nosotros() {
   return (
     <div>
-
+ <Catalogo/>
     </div>
   );
 }
--- frontend/src/pages/Productos.jsx
+++ frontend/src/pages/Productos.jsx
@@ -1,9 +1,11 @@
 import ProductList from "../components/cards/Productos/ProductList";
 
+import Catalogo from "../components/Catalogo/Catalogo";
 const Productos = ({data}) => {
     return(
         <div>
             <ProductList data={data}/>
+             <Catalogo/>
         </div>
     )
 };
